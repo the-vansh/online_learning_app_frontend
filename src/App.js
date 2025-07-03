e57a426f05fdcc05075a2e557bc29875d6377763
the-vansh <Contactvanshsaini@gmail.com>
2025-07-03 21:45:46 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <>
       <h1>Hello world</h1>
+      <p>This is a simple React application.</p>
+      <p>Welcome to the React world!</p>
+      <p>Enjoy coding with React!</p>
+      <p>Let's build something amazing together!</p>  
     </>
   );
 }
